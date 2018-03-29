48095a7bff8e5385c34cbffba5a2ca86c2262872
qinggge <zhu_huang@126.com>
2018-03-30 03:25:34 +0800

    xiedaimala_demojs

---
+++ demo.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+var fs = require('fs')
+
+var dirName = process.argv[2] // 你传的参数是从第 2 个开始的
+
+fs.mkdirSync("./" + dirName) // mkdir $1
+fs.mkdirSync("./" + dirName + "/css") // mkdir css
+fs.mkdirSync("./" + dirName + "/js") // mkdir js
+
+fs.writeFileSync("./" + dirName + "/index.html", "<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</hi>")
+fs.writeFileSync("./" + dirName + "/css/style.css", "h1{color: red;}")
+fs.writeFileSync("./" + dirName + "/js/main.js", "var string = 'Hello World'\nalert(srting)")
+
+process.exit(0) // exit 0
