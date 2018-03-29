6df4d12a6c26f2f4d3aca6b819e71e8d207ef09f
qinggge <zhu_huang@126.com>
2018-03-30 03:47:52 +0800

    update

--- demo.js
+++ demo.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 var fs = require('fs')
-
 var dirName = process.argv[2] // 你传的参数是从第 2 个开始的
+if(fs.existsSync("./"+dirName)){
+  console.log("dir exists!!")
+}else{
 
 fs.mkdirSync("./" + dirName) // mkdir $1
 fs.mkdirSync("./" + dirName + "/css") // mkdir css
@@ -10,5 +12,7 @@ fs.mkdirSync("./" + dirName + "/js") // mkdir js
 fs.writeFileSync("./" + dirName + "/index.html", "<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</hi>")
 fs.writeFileSync("./" + dirName + "/css/style.css", "h1{color: red;}")
 fs.writeFileSync("./" + dirName + "/js/main.js", "var string = 'Hello World'\nalert(srting)")
+console.log("sussess")
+}
 
 process.exit(0) // exit 0
--- demo.sh
+++ demo.sh
@@ -1,8 +1,13 @@
-mkdir $1
-cd $1
-mkdir css js
-touch index.html css/style.css js/main.js
-printf "<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>" >> index.html
-printf "hi{color: red;}" >> css/style.css
-printf "var string = 'Hello World'\nalert(string)" >> js/main.js
-exit
+if [ -d $1 ]; then
+  echo 'error: dir exists'
+  exit
+else
+  mkdir $1
+  cd $1
+  mkdir css js
+  printf "<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>" > index.html
+  printf "h1{color: red;}" > css/style.css
+  printf "var string = \"Hello World\"\nalert(string)" > js/main.js
+  echo 'success'
+  exit
+fi
